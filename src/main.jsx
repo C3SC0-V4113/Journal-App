9b9e8668fbe8964bfda4cad7ac0c0b78749393e8
CESCO <cescovalledev@gmail.com>
2023-06-27 23:24:11 -0600

    MaterialUI Added as a theme and a HOC

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import { JournalApp } from "./JournalApp.jsx";
-import "./styles.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import { AppRouter } from "./router/AppRouter.jsx";
+import { AppTheme } from "./theme";
+
+import "./styles.css";
 
 const router = createBrowserRouter(AppRouter);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AppTheme>
+      <RouterProvider router={router} />
+    </AppTheme>
   </React.StrictMode>
 );
