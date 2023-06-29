629de060e7fe62d9211c9e64c8f25fd4e5671daa
CESCO <cescovalledev@gmail.com>
2023-06-28 22:45:12 -0600

    Beggining shell of the app

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import { Box } from "@mui/material";
+
+const drawerWidth = 240;
+
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: "flex" }}>
+      {/**Navbar */}
+
+      {/**Sidebar */}
+
+      <Box component={"main"} sx={{ flexGrow: 1, p: 3 }}>
+        {/**Toolbar */}
+        {children}
+      </Box>
+    </Box>
+  );
+};
+
+JournalLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+  title: PropTypes.string,
+};
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,15 @@
 import { Typography } from "@mui/material";
 
+import { JournalLayout } from "../layout/JournalLayout";
+
 export const JournalPage = () => {
   return (
-    <div>
-      <Typography variant="h1">JournalPage</Typography>
-    </div>
+    <JournalLayout>
+      <Typography>
+        Tempor mollit Lorem nostrud quis qui eu ex consequat commodo. Consequat
+        do amet nostrud deserunt. Consectetur ea eu aliquip in occaecat irure
+        laboris. Commodo et pariatur esse sint aliquip.
+      </Typography>
+    </JournalLayout>
   );
 };
