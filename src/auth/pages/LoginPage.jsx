57a89c1e28b1ea5c10b0352c1a956bf8b7fbd31e
CESCO <cescovalledev@gmail.com>
2023-06-28 21:54:14 -0600

    Designing login pt 1

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,3 +1,45 @@
+import { Grid, TextField, Typography } from "@mui/material";
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction={"column"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
+    >
+      <Grid
+        item
+        className="box-shadow"
+        xs={3}
+        sx={{ backgroundColor: "white", padding: 3, borderRadius: 2 }}
+      >
+        <Typography variant="h5" sx={{ mb: 1 }}>
+          Login
+        </Typography>
+        <form>
+          <Grid container>
+            <Grid item xs={12} sx={{ marginTop: 2 }}>
+              <TextField
+                label="Correo"
+                type="email"
+                placeholder="mail@google.com"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sx={{ marginTop: 2 }}>
+              <TextField
+                label="Contraseña"
+                type="password"
+                placeholder="contraseña"
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
+  );
 };
