dc457a9023ec4b421e893006b11b3ff6cd6c3741
CESCO <cescovalledev@gmail.com>
2023-06-29 21:49:52 -0600

    Navbar worked

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,13 +1,13 @@
 import PropTypes from "prop-types";
 import { Box } from "@mui/material";
+import { NavBar } from "../components";
 
 const drawerWidth = 240;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
-      {/**Navbar */}
-
+      <NavBar drawerWidth={drawerWidth} />
       {/**Sidebar */}
 
       <Box component={"main"} sx={{ flexGrow: 1, p: 3 }}>
