d1c6567fed380b96763b2252ba2ae2d13f36fa22
CESCO <cescovalledev@gmail.com>
2023-06-29 22:15:49 -0600

    Sidebar added toolbar space

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { Box } from "@mui/material";
+import { Box, Toolbar } from "@mui/material";
 import { NavBar, SideBar } from "../components";
 
 const drawerWidth = 240;
@@ -10,7 +10,7 @@ export const JournalLayout = ({ children }) => {
       <NavBar drawerWidth={drawerWidth} />
       <SideBar drawerWidth={drawerWidth} />
       <Box component={"main"} sx={{ flexGrow: 1, p: 3 }}>
-        {/**Toolbar */}
+        <Toolbar />
         {children}
       </Box>
     </Box>
