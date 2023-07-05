4f685731ee37518028d3ce06dd55127cc62b59fb
CESCO <cescovalledev@gmail.com>
2023-07-04 21:23:26 -0600

    beggining redux in journal app

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import { JournalApp } from "./JournalApp.jsx";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import { AppRouter } from "./router/AppRouter.jsx";
 import { AppTheme } from "./theme";
+import { store } from "./store";
 
 import "./styles.css";
 
@@ -12,8 +14,10 @@ const router = createBrowserRouter(AppRouter);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppTheme>
-      <RouterProvider router={router} />
-    </AppTheme>
+    <Provider store={store}>
+      <AppTheme>
+        <RouterProvider router={router} />
+      </AppTheme>
+    </Provider>
   </React.StrictMode>
 );
