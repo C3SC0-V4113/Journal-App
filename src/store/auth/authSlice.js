4603d994527531b586da2e35c07344e0985ce750
CESCO <cescovalledev@gmail.com>
2023-07-05 22:23:55 -0600

    reducer knows about google auth

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -5,14 +5,28 @@ export const authSlice = createSlice({
   initialState: {
     status: "not-authenticated", // "checking", "not-authenticated", "authenticated"
     uid: null,
-    emal: null,
+    email: null,
     displayName: null,
     photoURL: null,
     errorMessage: null,
   },
   reducers: {
-    login: (state, action) => {},
-    logout: (state, payload) => {},
+    login: (state, { payload }) => {
+      state.status = "authenticated";
+      state.uid = payload.uid;
+      state.email = payload.email;
+      state.displayName = payload.displayName;
+      state.photoURL = payload.photoURL;
+      state.errorMessage = null;
+    },
+    logout: (state, { payload }) => {
+      state.status = "not-authenticated";
+      state.uid = null;
+      state.email = null;
+      state.displayName = null;
+      state.photoURL = null;
+      state.errorMessage = payload;
+    },
     checkingCredentials: (state) => {
       state.status = "checking";
     },
