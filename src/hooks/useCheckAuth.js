60aaf212b18cbab93740cb6d476c951c9dc74555
CESCO <cescovalledev@gmail.com>
2023-07-10 22:22:47 -0600

    useCheckAuth custom hook

---
+++ src/hooks/useCheckAuth.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
+
+import { login, logout } from "../store";
+import { FirebaseAuth } from "../firebase";
+
+export const useCheckAuth = () => {
+  const { status } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      if (!user) return dispatch(logout());
+      const { uid, email, displayName, photoURL } = user;
+      dispatch(login({ uid, email, displayName, photoURL }));
+    });
+  }, [dispatch]);
+
+  return {
+    status,
+  };
+};
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,23 +1,10 @@
-import { useDispatch, useSelector } from "react-redux";
 import { AuthRoutes } from "../auth";
 import { JournalRoutes } from "../journal";
 import { CheckingAuth } from "../ui";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { FirebaseAuth } from "../firebase";
-import { login, logout } from "../store";
+import { useCheckAuth } from "../hooks";
 
 export const AppRouter = () => {
-  const { status } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, async (user) => {
-      if (!user) return dispatch(logout());
-      const { uid, email, displayName, photoURL } = user;
-      dispatch(login({ uid, email, displayName, photoURL }));
-    });
-  }, [dispatch]);
+  const { status } = useCheckAuth();
 
   if (status === "checking") {
     return [
