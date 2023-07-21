040062ce0b12434f7120f1af0a04ec9cc312e9c2
CESCO <cescovalledev@gmail.com>
2023-07-20 22:19:42 -0600

    homework for logout testing done

--- src/test/store/auth/authSlice.test.js
+++ src/test/store/auth/authSlice.test.js
@@ -1,5 +1,10 @@
-import { authSlice } from "../../../store/auth/authSlice";
-import { initialState } from "../../fixtures/authFixtures";
+import { authSlice, login, logout } from "../../../store/auth/authSlice";
+import {
+  authenticatedState,
+  demoUser,
+  initialState,
+  notAuthenticatedState,
+} from "../../fixtures/authFixtures";
 
 describe("Pruebas en authSlice.js", () => {
   test('Debe de regresar el estado inicial y llamarse "auth"', () => {
@@ -9,5 +14,31 @@ describe("Pruebas en authSlice.js", () => {
     const state = authSlice.reducer(initialState, {});
 
     expect(state).toEqual(initialState);
+  });
+
+  test("debe realizar la autenticación", () => {
+    const state = authSlice.reducer(initialState, login(demoUser));
+    expect(state).toEqual(authenticatedState);
+  });
+
+  test("debe de realizar el logout sin argumentos", () => {
+    const state = authSlice.reducer(authenticatedState, logout());
+    expect(state).toEqual(notAuthenticatedState);
+  });
+
+  test("debe de realizar el logout y mostrar un mensaje de error", () => {
+    const errorMessage = "Credenciales son incorrectas";
+    const state = authSlice.reducer(
+      authenticatedState,
+      logout({ errorMessage })
+    );
+    expect(state).toEqual({
+      status: "not-authenticated",
+      uid: null,
+      email: null,
+      displayName: null,
+      photoURL: null,
+      errorMessage,
+    });
   });
 });
