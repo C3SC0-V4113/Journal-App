464030891ca4e4c291c74357492cc0d0e94f4ace
CESCO <cescovalledev@gmail.com>
2023-07-23 21:57:32 -0600

    thunks test journal for insertion done

--- src/test/store/journal/thunks.test.js
+++ src/test/store/journal/thunks.test.js
@@ -1,4 +1,11 @@
+import { collection, deleteDoc, getDocs } from "firebase/firestore/lite";
+import {
+  addNewEmptyNote,
+  savingNewNote,
+  setActiveNote,
+} from "../../../store/journal/journalSlice";
 import { startNewNote } from "../../../store/journal/thunks";
+import { FirebaseDB } from "../../../firebase";
 
 jest.mock("../../../firebase/providers");
 describe("pruebas en thunks.js de journal", () => {
@@ -12,5 +19,36 @@ describe("pruebas en thunks.js de journal", () => {
     getState.mockReturnValue({ auth: { uid } });
 
     await startNewNote()(dispatch, getState);
+
+    expect(dispatch).toHaveBeenCalledWith(savingNewNote());
+
+    expect(dispatch).toHaveBeenCalledWith(
+      addNewEmptyNote({
+        body: "",
+        title: "",
+        id: expect.any(String),
+        date: expect.any(Number),
+        imageUrls: expect.any(Array),
+      })
+    );
+
+    expect(dispatch).toHaveBeenCalledWith(
+      setActiveNote({
+        body: "",
+        title: "",
+        id: expect.any(String),
+        date: expect.any(Number),
+        imageUrls: expect.any(Array),
+      })
+    );
+
+    // Borrar de Firebase
+    const collectionRef = collection(FirebaseDB, `${uid}/journal/notas/`);
+
+    const docs = await getDocs(collectionRef);
+
+    const deletePromises = [];
+    docs.forEach((doc) => deletePromises.push(deleteDoc(doc.ref)));
+    await Promise.all(deletePromises);
   }, 20000);
 });
