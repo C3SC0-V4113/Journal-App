1836906870f843ac67d1cab79a89372e054461f9
CESCO <cescovalledev@gmail.com>
2023-07-24 23:02:37 -0600

    added logic to the firebase/config to work with environment variables

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Journal-App
+
 Journal Application based in Fernando Herrera course "React: De cero a experto"
+
+### Add your environment variables like .env.template but named ".env" por production and ".env.test" for testing
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -9,28 +9,23 @@ import { getEnvironments } from "../helpers";
 // console.log(import.meta.env);
 // console.log(process.env);
 
-const env = getEnvironments();
-console.log(env);
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID,
+} = getEnvironments();
 
 // Your web app's Firebase configuration
-// Dev/Prod
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAzIezw9D2d05vfboj8N460U1ziPNP_1GM",
-//   authDomain: "react-cursos-fh-51f42.firebaseapp.com",
-//   projectId: "react-cursos-fh-51f42",
-//   storageBucket: "react-cursos-fh-51f42.appspot.com",
-//   messagingSenderId: "811245014916",
-//   appId: "1:811245014916:web:d817f06d057ee3826997d1",
-// };
-
-//Testing
 const firebaseConfig = {
-  apiKey: "AIzaSyAohiw7YeWfeHsg9yOAiSSpNysGQVPPQnQ",
-  authDomain: "react-cursos-fh-testing.firebaseapp.com",
-  projectId: "react-cursos-fh-testing",
-  storageBucket: "react-cursos-fh-testing.appspot.com",
-  messagingSenderId: "809898106191",
-  appId: "1:809898106191:web:be9342863043aff9ffb93a",
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID,
 };
 
 // Initialize Firebase
