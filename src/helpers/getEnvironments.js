d32adade33f08bb1b24e98da2fc6f4798c5d457b
CESCO <cescovalledev@gmail.com>
2023-07-25 19:02:01 -0600

    trying to fix env error in prod 4

--- src/helpers/getEnvironments.js
+++ src/helpers/getEnvironments.js
@@ -1,7 +1,6 @@
 export const getEnvironments = () => {
-  import.meta.env;
-
-  return {
-    ...import.meta.env,
-  };
+  // import.meta.env;
+  // return {
+  //   ...import.meta.env,
+  // };
 };
